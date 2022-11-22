bf525845c4c22318a19bcdf227ecae88be9f9b57
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-22 08:12:56 +0000

    Update earthMagneticField.py

--- earthMagneticField.py
+++ earthMagneticField.py
@@ -10,17 +10,16 @@ RE = 6.370
 alpha = np.radians(9.6)
 
 
-def B(r, theta):
+def B(radius, angle):
     """Return the magnetic field vector at (r, theta)."""
-    fac = B_0 * (RE / r) ** 3
-    return -2 * fac * np.cos(theta + alpha), -fac * np.sin(theta + alpha)
+    fac = B_0 * (RE / radius) ** 3
+    return -2 * fac * np.cos(angle + alpha), -fac * np.sin(angle + alpha)
 
 
 # Grid of x, y points on a Cartesian grid
-nx, ny = 400, 400
-xmax, ymax = 80, 80
-x = np.linspace(-xmax, xmax, nx)
-y = np.linspace(-ymax, ymax, ny)
+nx, ny = 100, 100
+xmax, ymax = 100, 100
+x, y = np.linspace(-xmax, xmax, nx), np.linspace(-ymax, ymax, ny)
 X, Y = np.meshgrid(x, y)
 r, theta = np.hypot(X, Y), np.arctan2(Y, X)
 
@@ -28,26 +27,25 @@ r, theta = np.hypot(X, Y), np.arctan2(Y, X)
 Br, Btheta = B(r, theta)
 # Transform to Cartesian coordinates: NB make North point up, not to the right.
 c, s = np.cos(np.pi / 2 + theta), np.sin(np.pi / 2 + theta)
-Bx = -Btheta * s + Br * c
-By = Btheta * c + Br * s
+Bx, By = -Btheta * s + Br * c, Btheta * c + Br * s
+
+plt.style.use('dark_background')
 
 fig, ax = plt.subplots()
 
 # Plot the streamlines with an appropriate colormap and arrow style
 color = 2 * np.log(np.hypot(Bx, By))
-ax.streamplot(x, y, Bx, By, color=color, linewidth=0.5, cmap=plt.cm.inferno,
+ax.streamplot(x, y, Bx, By, color=color, linewidth=0.5, cmap=plt.cm.hot,
               density=2, arrowstyle='->', arrowsize=0.5)
 
 # Add a filled circle for the Earth; make sure it's on top of the streamlines.
-ax.add_patch(Circle((0, 0), RE, color='k', zorder=100))
+ax.add_patch(Circle((0, 0), RE, color='b', zorder=100,))
 ax = plt.gca()
 
 ax.spines['left'].set_color('none')
 ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_color('none')
 ax.spines['top'].set_color('none')
-ax.set_xlabel('$x$')
-ax.set_ylabel('$y$')
 ax.set_xlim(-xmax, xmax)
 ax.set_ylim(-ymax, ymax)
 ax.set_aspect('equal')
