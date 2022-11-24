4f8ea5d1e269d9b7bdfb49358dfa0d3220345bc5
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-24 10:59:41 +0000

    Update sportHeatmapper.py

--- sportHeatmapper.py
+++ sportHeatmapper.py
@@ -2,8 +2,9 @@ import gpxpy
 import pandas as pd
 import folium
 from xml.dom import minidom
-from os import listdir
-from os.path import join, isfile
+import os
+from pathlib import Path
+
 
 def process_gpx_to_df(file_name):
     gpx = gpxpy.parse(open(file_name))
@@ -29,25 +30,36 @@ def process_gpx_to_df(file_name):
             for point in segment.points:
                 points.append(tuple([point.latitude, point.longitude]))
 
-
     return gpx_df, points, type
 
-path = r"activities"
-for filename in listdir(path):  # iterates over all the files in 'path'
-    full_path = join(path, filename)  # joins the path with the filename
-    if isfile(full_path):  # validate that it is a file
-        with open(full_path) as f:  # open the file
-            gpx_df, points, type = process_gpx_to_df(filename)
+dirname = "activities"
 
-#gpx_df, points, type = process_gpx_to_df("2169765006.gpx")
-#print(type)
-#print(process_gpx_to_df("2169765006.gpx"))
+paths = Path(dirname).glob("*.gpx")
+
+for filename in os.listdir(dirname):
+    i = 0
+    if filename.endswith(".gpx"):
+        if i == 0:
+            gpx_df, points, type = process_gpx_to_df("activities/" + filename)
+            mymap = folium.Map(location=[gpx_df.Latitude.mean(), gpx_df.Longitude.mean()], zoom_start=6, tiles=None)
+            folium.PolyLine(points, color='red', weight=4.5, opacity=.5).add_to(mymap)
+            folium.TileLayer("openstreetmap", name="OpenStreet Map").add_to(mymap)
+            mymap.save('aee.html')
+            i += 1
+        else:
+            gpx_df, points, type = process_gpx_to_df("activities/" + filename)
+            folium.PolyLine(points, color='red', weight=4.5, opacity=.5).add_to(mymap)
+            folium.TileLayer("openstreetmap", name="OpenStreet Map").add_to(mymap)
 
 
-mymap = folium.Map( location=[ gpx_df.Latitude.mean(), gpx_df.Longitude.mean() ], zoom_start=6, tiles=None)
-folium.PolyLine(points, color='red', weight=4.5, opacity=.5).add_to(mymap)
-folium.TileLayer("openstreetmap", name="OpenStreet Map").add_to(mymap)
-#folium.TileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}", attr="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC”, name=’Nat Geo Map").add_to(mymap)
-#folium.TileLayer("http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg", attr="terrain-bcg", name="Terrain Map").add_to(mymap)
 
-mymap.save('aee.html')
+
+# gpx_df, points, type = process_gpx_to_df("2169765006.gpx")
+# print(type)
+# print(process_gpx_to_df("2169765006.gpx"))
+
+
+
+# folium.TileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}", attr="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC”, name=’Nat Geo Map").add_to(mymap)
+# folium.TileLayer("http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg", attr="terrain-bcg", name="Terrain Map").add_to(mymap)
+
