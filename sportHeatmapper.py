bf998803454d05d85e8b08a30aec3b34cc5188e1
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-06 21:31:33 +0000

    Update ...

--- sportHeatmapper.py
+++ sportHeatmapper.py
@@ -8,9 +8,11 @@ import plotly.express as px
 
 gpx_files = os.listdir("activities")
 
+gpx_files.remove(".DS_Store")
+
 coords_df = {}
 for i in range(len(gpx_files)):
-    gpx = gpxpy.parse(open('activities/' + gpx_files[i]))
+    gpx = gpxpy.parse(open("activities/" + gpx_files[i]))
     track_coords = [[point.latitude, point.longitude, point.elevation]
                     for track in gpx.tracks
                     for segment in track.segments
@@ -20,54 +22,11 @@ for i in range(len(gpx_files)):
 heatcoords_df = pd.concat(coords_df, ignore_index=True)
 heatcoords_df.columns = ['Latitude', 'Longitude', 'Altitude']
 
+runningheatmap = heatcoords_df.drop('Altitude', axis=1)
 
-
-def process_gpx_to_df(file_name):
-    gpx = gpxpy.parse(open(file_name))
-    file = minidom.parse(file_name)
-
-    # (1)make DataFrame
-    track = gpx.tracks[0]
-    segment = track.segments[0]
-    type = file.getElementsByTagName('item')
-    # Load the data into a Pandas dataframe (by way of a list)
-    data = []
-    segment_length = segment.length_3d()
-    for point_idx, point in enumerate(segment.points):
-        data.append([point.longitude, point.latitude, point.elevation,
-                     point.time, segment.get_speed(point_idx)])
-    columns = ["Longitude", "Latitude", "Altitude", "Time", "Speed"]
-    gpx_df = pd.DataFrame(data, columns=columns)
-
-    # 2(make points tuple for line)
-    points = []
-    for track in gpx.tracks:
-        for segment in track.segments:
-            for point in segment.points:
-                points.append(tuple([point.latitude, point.longitude]))
-
-    return gpx_df, points, type
-
-dirname = "activities"
-i = 0
-paths = Path(dirname).glob("*.gpx")
-
-for filename in os.listdir(dirname):
-
-    if filename.endswith(".gpx"):
-        if i == 0:
-            gpx_df, points, type = process_gpx_to_df("activities/" + filename)
-            mymap = folium.Map(location=[gpx_df.Latitude.mean(), gpx_df.Longitude.mean()], zoom_start=6, tiles=None)
-            folium.PolyLine(points, color='red', weight=4.5, opacity=.5).add_to(mymap)
-            folium.TileLayer("openstreetmap", name="OpenStreet Map").add_to(mymap)
-
-            i += 1
-        else:
-            print(mymap)
-            gpx_df, points, type = process_gpx_to_df("activities/" + filename)
-            symap = folium.Map(location=[gpx_df.Latitude.mean(), gpx_df.Longitude.mean()], zoom_start=6, tiles=None)
-            folium.PolyLine(points, color='red', weight=4.5, opacity=.5).add_to(mymap)
-            folium.TileLayer("openstreetmap", name="OpenStreet Map").add_to(mymap)
+mymap = folium.Map(location=[heatcoords_df.Latitude.mean(), heatcoords_df.Longitude.mean()], zoom_start=6, tiles=None)
+folium.PolyLine(runningheatmap, color='red', weight=2.5, opacity=.5).add_to(mymap)
+folium.TileLayer("openstreetmap", name="OpenStreet Map").add_to(mymap)
 
 
 mymap.save('Activities.html')
@@ -76,8 +35,5 @@ mymap.save('Activities.html')
 # print(type)
 # print(process_gpx_to_df("2169765006.gpx"))
 
-
-
 # folium.TileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}", attr="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC”, name=’Nat Geo Map").add_to(mymap)
 # folium.TileLayer("http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg", attr="terrain-bcg", name="Terrain Map").add_to(mymap)
-
