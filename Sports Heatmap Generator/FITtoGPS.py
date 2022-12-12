2fb543c6e65bb853a359b79fdf0633c06e3d47d4
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-12 12:20:07 +0000

    Update ...

--- Sports Heatmap Generator/FITtoGPS.py
+++ Sports Heatmap Generator/FITtoGPS.py
@@ -2,32 +2,34 @@ import fitparse
 import pandas as pd
 
 
-# def FITtoGPS(filename):
-array_lat = []
-array_lon = []
-# Load the FIT file
-fitfile = fitparse.FitFile("FIT/" + "3783334336.fit")
+def FITtoGPS(filename):
+    array_lat = []
+    array_lon = []
+    array_id = []
+    # Load the FIT file
+    fitfile = fitparse.FitFile("FIT/" + filename)
 
-for activity in fitfile.get_messages("sport"):
-    for data in activity:
-        print(activity.sport)
-# Iterate over all messages of type "record"
-# (other types include "device_info", "file_creator", "event", etc)
-for record in fitfile.get_messages("record"):
-    # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
-    for data in record:
+    for activity in fitfile.get_messages("sport"):
+        for data in activity:
 
-        # Print the name and value of the data (and the units if it has any)
-        if data.units:
-            if data.value is None:
-                break
-            if data.name == "position_lat":
-                lat = array_lat.append(data.value * 180 / (2 ** 31))
-            if data.name == "position_long":
-                lat = array_lon.append(data.value * 180 / (2 ** 31))
-            # print(" * {}: {} ({})".format(data.name, data.value, data.units))
-        # print(" * {}: {}".format(data.name, data.value))
+            array_id.append(str(data))
+    # Iterate over all messages of type "record"
+    # (other types include "device_info", "file_creator", "event", etc)
+    for record in fitfile.get_messages("record"):
+        # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
+        for data in record:
 
-df = pd.DataFrame({"Latitude": array_lat, "Longitude": array_lon})
-print(df)
-# return df
+            # Print the name and value of the data (and the units if it has any)
+            if data.units:
+                if data.value is None:
+                    break
+                if data.name == "position_lat":
+                    lat = array_lat.append(data.value * 180 / (2 ** 31))
+                if data.name == "position_long":
+                    lat = array_lon.append(data.value * 180 / (2 ** 31))
+                # print(" * {}: {} ({})".format(data.name, data.value, data.units))
+            # print(" * {}: {}".format(data.name, data.value))
+
+    df = pd.DataFrame({"Latitude": array_lat, "Longitude": array_lon})
+
+    return array_id, df
--- Sports Heatmap Generator/sportHeatmapper.py
+++ Sports Heatmap Generator/sportHeatmapper.py
@@ -11,14 +11,28 @@ fit_files = [file for file in location if file[-4:].lower() == '.fit']
 mymap = folium.Map()
 
 for file in fit_files:
-    result = f2g.FITtoGPS(file)
-    if len(result) > 100:
+    sport, result = f2g.FITtoGPS(file)
+    if len(result) > 10:
     #df = pd.read_csv("FIT files/" + file)
     #filteredd_df = df.loc[df['Type'] == 'Data']
     #filtered_df = filteredd_df.loc[df['Message'] == 'record'].iloc[5:]
     #result = filtered_df[["Value 1", "Value 2"]]
     #results = result.rename(columns={"Value 1": "Latitude", "Value 2": "Longitude"})
-        folium.PolyLine(result.astype(float), color='green', weight=3, opacity=1).add_to(mymap)
+        if bool(sport):
+            if sport[0] == "name: Walk":
+                folium.PolyLine(result.astype(float), color='green', weight=1.5, opacity=1).add_to(mymap)
+            elif sport[0] == "name: Bike":
+                folium.PolyLine(result.astype(float), color='blue', weight=1.5, opacity=1).add_to(mymap)
+            elif sport[0] == "name: Run":
+                folium.PolyLine(result.astype(float), color='red', weight=1.5, opacity=1).add_to(mymap)
+            elif sport[0] == "name: Trail Run":
+                folium.PolyLine(result.astype(float), color='darkred', weight=1.5, opacity=1).add_to(mymap)
+            elif sport[0] == "name: Open Water":
+                folium.PolyLine(result.astype(float), color='cadetblue', weight=1.5, opacity=1).add_to(mymap)
+            elif sport[0] == "name: Hike":
+                folium.PolyLine(result.astype(float), color='purple', weight=1.5, opacity=1).add_to(mymap)
+            else:
+                folium.PolyLine(result.astype(float), color='red', weight=1.5, opacity=1).add_to(mymap)
 
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
 #     print(result)
