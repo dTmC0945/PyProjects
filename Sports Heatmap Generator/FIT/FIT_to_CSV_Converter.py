5ca23a74c9ffc18ee26915075e5c6603591ecaef
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-12 10:23:05 +0000

    Update FIT_to_CSV_Converter.py

--- Sports Heatmap Generator/FIT/FIT_to_CSV_Converter.py
+++ Sports Heatmap Generator/FIT/FIT_to_CSV_Converter.py
@@ -1,22 +1,25 @@
 import fitparse
 import pandas as pd
 
+
+df = pd.DataFrame([])
+
 # Load the FIT file
 fitfile = fitparse.FitFile("6104083085.fit")
 
-df = pd.DataFrame()
 # Iterate over all messages of type "record"
 # (other types include "device_info", "file_creator", "event", etc)
 for record in fitfile.get_messages("record"):
-
     # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
     for data in record:
+        k = data.value
+        print(k)
+
 
         # Print the name and value of the data (and the units if it has any)
-        if data.units:
-            frame = pd.DataFrame(data.units)
-            print(" * {}: {} ({})".format(data.name, data.value, data.units))
-        else:
-            print(" * {}: {}".format(data.name, data.value))
+        # if data.units:
+        #     print(" * {}: {} ({})".format(data.name, data.value, data.units))
+        # else:
+        #     print(" * {}: {}".format(data.name, data.value))
 
     print("---")
