8a0440a2a9f73fcb5d9b4174b0511b7e1577ab20
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-14 12:24:42 +0000

    Update main.py

--- Game of Life/main.py
+++ Game of Life/main.py
@@ -1,19 +1,38 @@
 import sys
 import pygame
+import numpy as np
+
+
+class GameOfLife:
+    def __init__(self, surface, width=640, height=480, scale=10, offset=1, active_color=(255, 255, 255),
+                 inactive_color=(50, 50, 50)):
+        self.surface = surface
+        self.width = width
+        self.height = height
+        self.scale = scale
+        self.offset = offset
+        self.active_color = active_color
+        self.inactive_color = inactive_color
+
 
 pygame.init()
+pygame.display.set_caption("Conway's Game of Life")
 
 WIDTH = 640
 HEIGHT = 480
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+conway = GameOfLife(screen, scale=13)
+
+clock = pygame.time.Clock()
+fps = 60
+
 while True:
+    clock.tick(fps)
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
-
-    screen.fill((255,255,255))
-
-    pygame.draw.rect(screen, (25, 25, 112), [(640/2) - 50, (480/2) - 50, 100, 100])
-    pygame.display.update()
+            pygame.quit()
+            sys.exit
