6b95e46a0c4a4bb5f8a0bd2c97ef3498677a7fe9
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-19 16:19:48 +0100

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -5,9 +5,7 @@ from matplotlib import animation
 # X, Y = np.sin(np.arange(0, 0.01, 2*np.pi)), np.cos(np.arange(0, 0.01, 2*np.pi))
 
 X, Y = 0, 0
-U, V = 3*np.sin(np.pi - 2*np.pi / 3), 3*np.sin(np.pi - 2*np.pi / 3)
-
-
+U, V = 3 * np.sin(np.pi - 2 * np.pi / 3), 3 * np.sin(np.pi - 2 * np.pi / 3)
 
 fig, ax = plt.subplots(1, 1)
 Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='inches')
@@ -20,8 +18,8 @@ def update_quiver(num, Q, X, Y):
     fixed increment on each frame
     """
 
-    U = np.sin(X + num * 0.1)
-    V = np.cos(Y + num * 0.1)
+    U = (X + np.sin(num * np.pi / 5) * 10)
+    V = (Y + np.cos(num * np.pi / 5) * 10)
 
     Q.set_UVC(U, V)
 
@@ -30,6 +28,6 @@ def update_quiver(num, Q, X, Y):
 
 # you need to set blit=False, or the first set of arrows never gets
 # cleared on subsequent frames
-anim = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y), interval=10, blit=False)
+anim = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y), interval=50, blit=False)
 fig.tight_layout()
 plt.show()
