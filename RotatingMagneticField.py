22b6c9813803fbdcac0d1b4fd7fd971d4c5e6214
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-22 15:36:44 +0100

    Update ...

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -18,7 +18,7 @@ def update_quiver(num, Q, X, Y, phase):
     fixed increment on each frame
     """
 
-    U = X + (np.sin(num * 2 * np.pi / 100 - phase) * 5)
+    U = X + 0
     V = Y + (np.cos(num * 2 * np.pi / 100 - phase) * 5)
 
     Q.set_UVC(U, V)
