0a710cc1b6b8d65f9b3824027120c6328c0934e2
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-22 15:41:50 +0100

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -5,7 +5,7 @@ from matplotlib import animation
 # X, Y = np.sin(np.arange(0, 0.01, 2*np.pi)), np.cos(np.arange(0, 0.01, 2*np.pi))
 
 X, Y = 0, 0
-U, V = 1, 0
+U, V = 1*np.cos(np.pi/4), 1*np.sin(np.pi/4)
 fig, ax = plt.subplots(1, 1)
 
 Q = ax.quiver(X, Y, U, V, pivot='tail', color='b', units='inches')
@@ -18,8 +18,8 @@ def update_quiver(num, Q, X, Y, phase):
     fixed increment on each frame
     """
 
-    U = X + (np.sin(num * 2 * np.pi / 100 - phase) * 5)*np.sin(0)
-    V = Y + (np.cos(num * 2 * np.pi / 100 - phase) * 5)*np.cos(0)
+    U = X + (np.sin(num * 2 * np.pi / 100 - phase) * 5)*np.sin(np.pi/4)
+    V = Y + (np.cos(num * 2 * np.pi / 100 - phase) * 5)*np.cos(np.pi/4)
 
     Q.set_UVC(U, V)
 
