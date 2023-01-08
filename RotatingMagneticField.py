6c112520a5530b0c97646d943e1f5c0b9ade6aea
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-08 20:31:37 +0300

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -15,12 +15,17 @@ Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='inches')
 ax.set_xlim(-5, 5), ax.set_ylim(-5, 5)
 
 
-def update_quiver(num, Q, X, Y, phase, amplitude):
+def update_quiver(num, Q, X, Y):
     """updates the horizontal and vertical vector components by a
     fixed increment on each frame
     """
-    U = X + (np.sin(num * 2 * np.pi / 100 - phase)) * np.sin(phase) * amplitude
-    V = Y + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * amplitude
+    U = [X[0] + (np.sin(num * 2 * np.pi / 100 - np.pi/2)) * np.sin(np.pi/2),
+         X[1] + (np.sin(num * 2 * np.pi / 100 - np.pi/2 - 2 * np.pi/3)) * np.sin(np.pi/2 + 2 * np.pi/3),
+         X[2] + (np.sin(num * 2 * np.pi / 100 - np.pi/2 + 2 * np.pi/3)) * np.sin(np.pi/2 - 2 * np.pi/3)]
+
+    V = [Y[0] + (np.sin(num * 2 * np.pi / 100 - np.pi/2)) * np.cos(np.pi/2),
+         Y[1] + (np.sin(num * 2 * np.pi / 100 - np.pi/2 - 2 * np.pi/3)) * np.cos(np.pi/2 + 2 * np.pi/3),
+         Y[2] + (np.sin(num * 2 * np.pi / 100 - np.pi/2 + 2 * np.pi/3)) * np.cos(np.pi/2 - 2 * np.pi/3)]
 
     Q.set_UVC(U, V)
 
@@ -31,7 +36,7 @@ Inner_Stator = plt.Circle((0, 0), 5, color='k', fill=False)
 
 # you need to set blit=False, or the first set of arrows never gets
 # cleared on subsequent frames
-ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, 0, 5), interval=1)
+ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y), interval=1)
 fig.tight_layout()
 ax.add_patch(Inner_Stator)
 ax.set_box_aspect(1)
