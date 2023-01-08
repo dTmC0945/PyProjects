627254b70b09a6286c6e35d9046d8f79f161af6e
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-08 21:37:11 +0300

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -2,15 +2,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-phase = 10
+# number of phases in the system
+phase = 4
 
 
-def quiverGeneration(phase):
+def quiverGeneration(value):
     x_int, y_int, x_dest, y_dest = [], [], [], []
-    for i in range(phase):
+    for i in range(value):
         x_int.append(0), y_int.append(0)
-        x_dest.append(1 * np.cos(i * np.pi / phase))
-        y_dest.append(1 * np.sin(i * np.pi / phase))
+        x_dest.append(1 * np.cos(i * np.pi / value))
+        y_dest.append(1 * np.sin(i * np.pi / value))
     return x_int, y_int, x_dest, y_dest
 
 
@@ -18,22 +19,21 @@ X, Y, U, V = quiverGeneration(phase)
 
 fig, ax = plt.subplots(1, 1)
 
-Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='xy', scale=1)
+Q = ax.quiver(X, Y, U, V, pivot='tail', color=(.4, .5, .6), units='xy', scale=1)
 
 ax.set_xlim(-1, 1), ax.set_ylim(-1, 1)
 
 
-def update_quiver(num, Q, X, Y, phase):
+def update_quiver(num, Q, X, Y, value):
     """updates the horizontal and vertical vector components by a
     fixed increment on each frame
     """
     U, V = [], []
-    for i in range(phase):
-        U.append(X[i] + (np.sin(num * np.pi / 50 - (i * np.pi / phase)) *
-                         np.sin(i * np.pi / phase)))
-        V.append(Y[i] + (np.sin(num * np.pi / 50 - (i * np.pi / phase)) *
-                         np.cos(i * np.pi / phase)))
-
+    for i in range(value):
+        U.append(X[i] + (np.sin(num * np.pi / 50 - (i * np.pi / value)) *
+                         np.sin(i * np.pi / value)))
+        V.append(Y[i] + (np.sin(num * np.pi / 50 - (i * np.pi / value)) *
+                         np.cos(i * np.pi / value)))
 
     Q.set_UVC(U, V)
 
@@ -41,13 +41,13 @@ def update_quiver(num, Q, X, Y, phase):
 
 
 Inner_Stator = plt.Circle((0, 0), 1, color='k', fill=False)
-Middle_Stator = plt.Circle((0, 0), 0.5, color='k', fill=False, linestyle='--', alpha =0.25)
+Middle_Stator = plt.Circle((0, 0), 0.5, color='k', fill=False, linestyle='--', alpha=0.25)
 
 Central = plt.Circle((0, 0), 0.05, color='k', fill=True)
 
 # you need to set blit=False, or the first set of arrows never gets
 # cleared on subsequent frames
-ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, phase), interval=10)
+ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, phase), interval=1)
 fig.tight_layout()
 ax.add_patch(Inner_Stator)
 ax.add_patch(Middle_Stator)
