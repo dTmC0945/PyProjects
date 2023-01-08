f69b13d62479543aeb3b79f1a9df280e8c149edd
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-08 19:31:34 +0300

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -13,17 +13,21 @@ Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='inches')
 ax.set_xlim(-5, 5), ax.set_ylim(-5, 5)
 
 
-def update_quiver(num, Q, X, Y, phase, Amplitude):
+def update_quiver(num, Q1, X, Y, phase, Amplitude):
     """updates the horizontal and vertical vector components by a
     fixed increment on each frame
     """
 
+
     U = X + (np.sin(num * 2 * np.pi / 100 - phase)) * np.sin(phase) * Amplitude
     V = Y + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * Amplitude
 
-    Q.set_UVC(U, V)
+    U2 = X + (np.sin(num * 2 * np.pi / 100 - phase + 2*np.pi/3)) * np.sin(phase) * Amplitude
+    V2 = Y + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * Amplitude
 
-    return Q,
+    Q1.set_UVC(U, V)
+
+    return Q1,
 
 
 # you need to set blit=False, or the first set of arrows never gets
