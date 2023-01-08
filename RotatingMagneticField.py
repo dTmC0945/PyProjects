5d6ea141e746c06604eea5e3016dd82a26563a1c
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-08 20:26:14 +0300

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -10,7 +10,7 @@ U, V = [1*np.cos(np.pi/2), 1*np.cos(np.pi/2 + 2*np.pi/3), 1*np.cos(np.pi/2 - 2*n
 
 fig, ax = plt.subplots(1, 1)
 
-Q = ax.quiver(X, Y, U, V, scale=1, pivot='tail', color='r', units='inches')
+Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='inches')
 
 ax.set_xlim(-5, 5), ax.set_ylim(-5, 5)
 
@@ -19,8 +19,8 @@ def update_quiver(num, Q, X, Y, phase, amplitude):
     """updates the horizontal and vertical vector components by a
     fixed increment on each frame
     """
-    U = X[1] + (np.sin(num * 2 * np.pi / 100 - phase)) * np.sin(phase) * amplitude
-    V = Y[1] + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * amplitude
+    U = X + (np.sin(num * 2 * np.pi / 100 - phase)) * np.sin(phase) * amplitude
+    V = Y + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * amplitude
 
     Q.set_UVC(U, V)
 
@@ -31,9 +31,8 @@ Inner_Stator = plt.Circle((0, 0), 5, color='k', fill=False)
 
 # you need to set blit=False, or the first set of arrows never gets
 # cleared on subsequent frames
-#ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, 0, 5), interval=1)
-#ani2 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, 2*np.pi/3, 5), interval=1)
-#fig.tight_layout()
+ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, 0, 5), interval=1)
+fig.tight_layout()
 ax.add_patch(Inner_Stator)
 ax.set_box_aspect(1)
 plt.show()
