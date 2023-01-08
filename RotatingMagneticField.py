fdff230f7075f735af2dd8867f41b1a7e809b718
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-08 19:44:53 +0300

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -13,23 +13,24 @@ Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='inches')
 ax.set_xlim(-5, 5), ax.set_ylim(-5, 5)
 
 
-def update_quiver(num,Q, X, Y, phase, Amplitude):
+def update_quiver(num, X, Y, phase, amplitude):
     """updates the horizontal and vertical vector components by a
     fixed increment on each frame
     """
-    U = X + (np.sin(num * 2 * np.pi / 100 - phase)) * np.sin(phase) * Amplitude
-    V = Y + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * Amplitude
+    U = X + (np.sin(num * 2 * np.pi / 100 - phase)) * np.sin(phase) * amplitude
+    V = Y + (np.sin(num * 2 * np.pi / 100 - phase)) * np.cos(phase) * amplitude
 
-    U2 = X + (np.sin(num * 2 * np.pi / 100 - (phase + 2*np.pi/3))) * np.sin(phase + 2*np.pi/3) * Amplitude
-    V2 = Y + (np.sin(num * 2 * np.pi / 100 - (phase + 2*np.pi/3))) * np.cos(phase + 2*np.pi/3) * Amplitude
+    U2 = X + (np.sin(num * 2 * np.pi / 100 - (phase + 2*np.pi/3))) * np.sin(phase + 2*np.pi/3) * amplitude
+    V2 = Y + (np.sin(num * 2 * np.pi / 100 - (phase + 2*np.pi/3))) * np.cos(phase + 2*np.pi/3) * amplitude
 
-    Q.set_UVC(U + U2, V + V2)
+    Q.set_UVC(U2, V2)
+    Q
 
     return Q
 
 
 # you need to set blit=False, or the first set of arrows never gets
 # cleared on subsequent frames
-anim1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, 2*np.pi/3, 5), interval=5, blit=False)
+anim1 = animation.FuncAnimation(fig, update_quiver, fargs= (X, Y, 2*np.pi/3, 5), interval=5, blit=False)
 fig.tight_layout()
 plt.show()
