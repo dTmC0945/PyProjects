f582efd8df46a52b97b75ebdbc3c7c8d430cb2a4
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-08 21:21:47 +0300

    Update RotatingMagneticField.py

--- RotatingMagneticField.py
+++ RotatingMagneticField.py
@@ -2,16 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-# X, Y = np.sin(np.arange(0, 0.01, 2*np.pi)), np.cos(np.arange(0, 0.01, 2*np.pi))
-
 phase = 10
+
 
 def quiverGeneration(phase):
     x_int, y_int, x_dest, y_dest = [], [], [], []
     for i in range(phase):
         x_int.append(0), y_int.append(0)
-        x_dest.append(0.125 * np.cos(i * np.pi / phase))
-        y_dest.append(0.125 * np.sin(i * np.pi / phase))
+        x_dest.append(1 * np.cos(i * np.pi / phase))
+        y_dest.append(1 * np.sin(i * np.pi / phase))
     return x_int, y_int, x_dest, y_dest
 
 
@@ -19,9 +18,9 @@ X, Y, U, V = quiverGeneration(phase)
 
 fig, ax = plt.subplots(1, 1)
 
-Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='inches',scale=None)
+Q = ax.quiver(X, Y, U, V, pivot='tail', color='r', units='xy', scale=None)
 
-ax.set_xlim(-5, 5), ax.set_ylim(-5, 5)
+ax.set_xlim(-1, 1), ax.set_ylim(-1, 1)
 
 
 def update_quiver(num, Q, X, Y, phase):
@@ -35,17 +34,20 @@ def update_quiver(num, Q, X, Y, phase):
         V.append(Y[i] + (np.sin(num * np.pi / 25 - (i * np.pi / phase)) *
                          np.cos(i * np.pi / phase)))
 
+
     Q.set_UVC(U, V)
 
     return Q
 
 
-Inner_Stator = plt.Circle((0, 0), 5, color='k', fill=False)
+Inner_Stator = plt.Circle((0, 0), 1, color='k', fill=False)
+Central = plt.Circle((0, 0), 0.01, color='k', fill=True)
 
 # you need to set blit=False, or the first set of arrows never gets
 # cleared on subsequent frames
 ani1 = animation.FuncAnimation(fig, update_quiver, fargs=(Q, X, Y, phase), interval=1)
 fig.tight_layout()
 ax.add_patch(Inner_Stator)
+ax.add_patch(Central)
 ax.set_box_aspect(1)
 plt.show()
