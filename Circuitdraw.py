8b3f1e83a25311886418e68de339a63137009ab9
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-01-14 10:36:43 +0300

    Update Circuitdraw.py

--- Circuitdraw.py
+++ Circuitdraw.py
@@ -1,8 +1,19 @@
 import schemdraw
 import schemdraw.elements as elm
 
-
 with schemdraw.Drawing() as d:
-    d.add(elm.Resistor())
-    d.add(elm.Capacitor())
-    d.add(elm.Diode())
+    d += (Q := elm.IcDIP(pins=8)
+                 .label('Offset Null', loc='p1', fontsize=10)
+                 .label('Inverting Input', loc='p2', fontsize=10)
+                 .label('Non-inverting Input', loc='p3', fontsize=10)
+                 .label('V-', loc='p4', fontsize=10)
+                 .label('Offset Null', loc='p5', fontsize=10)
+                 .label('Output', loc='p6', fontsize=10)
+                 .label('V+', loc='p7', fontsize=10)
+                 .label('NC', loc='p8', fontsize=10))
+    d += elm.Line().at(Q.p2_in).length(d.unit/5)
+    d += (op := elm.Opamp().anchor('in1').scale(.8))
+    d += elm.Line().at(Q.p3_in).length(d.unit/5)
+    d += elm.Wire('c', k=.3).at(op.out).to(Q.p6_in)
+    d += elm.Wire('-|').at(Q.p4_in).to(op.n1)
+    d += elm.Wire('-|').at(Q.p7_in).to(op.n2)
