57ad07d8ecc3ccfeb2960d4cab2c3e5a2fb9a69f
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-04-28 12:52:58 +0200

    Update ...

--- A Game of Chess/Chess/ChessEngine.py
+++ A Game of Chess/Chess/ChessEngine.py
@@ -3,9 +3,22 @@ This class is responsible for storing all the information about the current stat
 It will also be responsible for determining the valid moves of the current state. It will also keep a move log.
 """
 
-class GameState():
+
+class GameState:
     def __int__(self):
+        # Board is an 8 by 8 2D list, with each element of the list has 2 characters.
+        # The first character represents the color of the piece "b" or "w".
+        # The second character represents the type of the piece, "K", "Q", "R", "B", "N" or "P".
+        # "--" represents an empty space with no piece.
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        self.whiteToMove = True
+        self.moveLog = []
--- A Game of Chess/Chess/ChessMain.py
+++ A Game of Chess/Chess/ChessMain.py
@@ -1,3 +1,12 @@
 """
 This is our main driver file, It will be responsible for handling user input and displaying the current GameState object.
 """
+
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8 # Chessboard is an 8 by 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # For animation use.
+IMAGES = {}
