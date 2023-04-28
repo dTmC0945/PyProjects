4b678c6c68880cbceddd69289a5d247090f81ddf
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-04-28 12:59:34 +0200

    Update ...

--- A Game of Chess/Chess/ChessMain.py
+++ A Game of Chess/Chess/ChessMain.py
@@ -3,10 +3,21 @@ This is our main driver file, It will be responsible for handling user input and
 """
 
 import pygame as p
-from Chess import ChessEngine
+from ChessEngine import *
 
 WIDTH = HEIGHT = 512
-DIMENSION = 8 # Chessboard is an 8 by 8
+DIMENSION = 8  # Chessboard is an 8 by 8
 SQ_SIZE = HEIGHT // DIMENSION
-MAX_FPS = 15 # For animation use.
+MAX_FPS = 15  # For animation use.
 IMAGES = {}
+
+"""
+Initialise a global dictionary of images. This will be called exactly once in the main().
+"""
+
+
+def loadImages():
+    pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bR", "bN", "bB", "bK", "bQ"]
+    for piece in pieces:
+        IMAGES[piece] = p.image.load("images/" + piece + "wp.png")
+    # NOTE: can access an image by saying "IMAGES["wp"]
