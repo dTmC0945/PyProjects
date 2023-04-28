6b47fc218ae6166c01ad8534b68f6fc114b1b380
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2023-04-28 12:45:44 +0200

    Update ...

--- A Game of Chess/Chess/ChessEngine.py
+++ A Game of Chess/Chess/ChessEngine.py
@@ -2,3 +2,10 @@
 This class is responsible for storing all the information about the current state of a chess game.
 It will also be responsible for determining the valid moves of the current state. It will also keep a move log.
 """
+
+class GameState():
+    def __int__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+        ]
